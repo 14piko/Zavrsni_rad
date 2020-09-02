f7cb7103038ac4c44061837ed6bc27b414287e73
Mirko Ereš <mirko.eres1@gmail.com>
2020-09-02 12:39:31 +0200

    Priprema konfiguracije s podacima za bazu

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,8 +1,26 @@
 <?php
 
+$dev=$_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false
 
+if($dev){
+    $baza=[
+        'server' => 'localhost',
+        'baza' => 'edunovapp21',
+        'korisnik' => 'edunova',
+        'lozinka' => 'edunova'
+    ];
+}else{
+    $baza=[
+        'server' => 'localhost',
+        'baza' => 'polaznik_pp21',
+        'korisnik' => 'polaznik_edunova',
+        'lozinka' => 'edunova'
+    ];  
+}
+    
 return [
-    'dev' => $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false,
+    'dev' => $dev,
     'nazivAPP' => 'Edunova APP',
     'url' => 'http://polaznik01.edunova.hr/'
+    'baza' => $baza
 ];
