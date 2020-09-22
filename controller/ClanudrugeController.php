6e890ba0b9a34b69def76c5399eebe2414e9ec0f
Mirko Ereš <mirko.eres1@gmail.com>
2020-09-22 14:59:41 +0200

    Dodavanje novog člana

--- controller/ClanudrugeController.php
+++ controller/ClanudrugeController.php
@@ -18,18 +18,33 @@ class ClanudrugeController extends AutorizacijaController
         public function novo()
         {
             if ($_SERVER['REQUEST_METHOD']==='GET'){
-            $this->view->render($this->viewDir . 'novo',[
-                'poruka'=>'Unesite tražene podatke!'
+            $this->novoView('Unesite tražene podatke!',[
+                'ime'=>'',
+                'prezime'=>'',
+                'oib'=>'',
+                'brojdozvole'=>''
             ]);
             return;
         }
 
             //radi se o POST i moram kontrolirati prije unosa u bazu
             //kontroler mora kontrolirati vrijednosti prije nego se ode u bazu
-        
+            $clan=$_POST;
+
+            if(strlen(trim($clan['ime']))===0){
+                $this->novoView('Obavezan unos imena!',$_POST);
+                return;
+            }
+
+
+
             Clanudruge::dodajNovi($_POST);
 
-            $this->index();
+           //ovo ispod unese i prebaci te na popis svih članova
+            //$this->index();
+
+            //ovaj unese i ostavi te sa svim podacima na trenutnoj stranici
+            $this->novoView('Uneseno! Nastavite s unosom novih podataka!',$_POST);
         
     }
 
@@ -43,6 +58,14 @@ class ClanudrugeController extends AutorizacijaController
         {
 
 
+        }
+
+        private function novoView($poruka,$clan)
+        {
+            $this->view->render($this->viewDir . 'novo',[
+                'poruka'=>$poruka,
+                'clanudruge'=> $clan
+            ]);
         }
         
 }
