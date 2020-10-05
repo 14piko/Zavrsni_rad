5c20da07271ac54058c7ab04ee593bd0038f5cea
Mirko Ereš <mirko.eres1@gmail.com>
2020-10-05 16:31:12 +0200

    Pecanje create kontrole

--- controller/PecanjeController.php
+++ controller/PecanjeController.php
@@ -26,19 +26,18 @@ class PecanjeController extends AutorizacijaController
         if ($_SERVER['REQUEST_METHOD']==='GET'){
             $pecanje=new stdClass();
             $pecanje->datum='';
-            $pecanje->clan=0;
+            $pecanje->clanovi=0;
             $pecanje->riba=0;
             $pecanje->kolicina='';
             $pecanje->tezina='';
             $pecanje->rijeka=0;
-            $this->novoView('Unesite tražene podatke',$pecanje,
-            Clanudruge::ucitajSve(1,'%'),
-            Riba::ucitajSve(),
-            Rijeka::ucitajSve()
-            );
+            $this->novoView('Unesite tražene podatke',$pecanje);
             return;
         }
         $pecanje=(object)$_POST;
+        if(!$this->kontrolaClan($pecanje,'novoView')){return;};
+        if(!$this->kontrolaRiba($pecanje,'novoView')){return;};
+        if(!$this->kontrolaRijeka($pecanje,'novoView')){return;};
         Pecanje::dodajNovi($_POST);
         $this->index();
     }
@@ -57,14 +56,14 @@ class PecanjeController extends AutorizacijaController
             $this->index();
         }
 
-        private function novoView($poruka,$pecanje,$clanovi,$ribe,$rijeke)
+        private function novoView($poruka,$pecanje)
         {
            $this->view->render($this->viewDir . 'novo',[
                'poruka'=>$poruka,
                'pecanje'=>$pecanje,
-               'ribe'=>$ribe,
-               'rijeke'=>$rijeke,
-               'clanovi'=>$clanovi
+               'ribe'=>Riba::ucitajSve(),
+               'rijeke'=>Rijeka::ucitajSve(),
+               'clanovi'=>Clanudruge::ucitajSve(1,'%')
               
            ]);
         }
@@ -73,9 +72,45 @@ class PecanjeController extends AutorizacijaController
         {
             $this->view->render($this->viewDir . 'promjena',[
                 'poruka'=>$poruka,
-                'pecanje'=>$pecanje
+                'pecanje'=>$pecanje,
+                'ribe'=>Riba::ucitajSve(),
+               'rijeke'=>Rijeka::ucitajSve(),
+               'clanovi'=>Clanudruge::ucitajSve(1,'%')
             ]);
         }
+    
+        private function kontrolaClan($pecanje,$view)
+        {
+            if($pecanje->clanovi==0){
+                $this->$view('Obavezan odabir člana!',$pecanje);
+                return false;
+            }
+            
+                //na kraju uvijek vrati true
+                return true;
+            }
 
+
+    private function kontrolaRiba($pecanje,$view)
+        {
+            if($pecanje->riba==0){
+            $this->$view('Obavezan odabir ribe!',$pecanje);
+            return false;
+            }
+                
+            //na kraju uvijek vrati true
+            return true;
+        }
+
+    private function kontrolaRijeka($pecanje,$view)
+        {
+            if($pecanje->rijeka==0){
+            $this->$view('Obavezan odabir rijeke!',$pecanje);
+            return false;
+            }
+                    
+            //na kraju uvijek vrati true
+            return true;
+        }
     }
 
