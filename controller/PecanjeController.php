020932d447e1d9b6842af5e8f4383e49d1b859ef
Mirko Ereš <mirko.eres1@gmail.com>
2020-10-05 15:03:33 +0200

    Pecanje Create priprema 1

--- controller/PecanjeController.php
+++ controller/PecanjeController.php
@@ -21,7 +21,28 @@ class PecanjeController extends AutorizacijaController
     
         }
 
-            
+    public function novo()
+    {
+        if ($_SERVER['REQUEST_METHOD']==='GET'){
+            $pecanje=new stdClass();
+            $pecanje->datum='';
+            $pecanje->clan=0;
+            $pecanje->riba=0;
+            $pecanje->kolicina='';
+            $pecanje->tezina='';
+            $pecanje->rijeka=0;
+            $this->novoView('Unesite tražene podatke',$pecanje,
+            Clanudruge::ucitajSve(1,'%'),
+            Riba::ucitajSve(),
+            Rijeka::ucitajSve()
+            );
+            return;
+        }
+        $pecanje=(object)$_POST;
+        Pecanje::dodajNovi($_POST);
+        $this->index();
+    }
+    
         
 
 
@@ -36,14 +57,24 @@ class PecanjeController extends AutorizacijaController
             $this->index();
         }
 
-        private function novoView($poruka,$clan)
+        private function novoView($poruka,$pecanje,$clanovi,$ribe,$rijeke)
         {
-           
+           $this->view->render($this->viewDir . 'novo',[
+               'poruka'=>$poruka,
+               'pecanje'=>$pecanje,
+               'ribe'=>$ribe,
+               'rijeke'=>$rijeke,
+               'clanovi'=>$clanovi
+              
+           ]);
         }
 
-        private function promjenaView($poruka,$clan)
+        private function promjenaView($poruka,$pecanje)
         {
-           
+            $this->view->render($this->viewDir . 'promjena',[
+                'poruka'=>$poruka,
+                'pecanje'=>$pecanje
+            ]);
         }
 
     }
